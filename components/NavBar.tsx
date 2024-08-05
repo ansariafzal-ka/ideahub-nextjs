3cedfeb2fbff6c3665e11135d6ae27ffc2226561
ansariafzal <ansariafzal2112@gmail.com>
2024-08-05 13:18:38 +0530

    NavBar made sticky

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <nav className="px-6 py-3 border-b">
+    <nav className="px-6 py-3 border-b sticky top-0 left-0 bg-background z-30">
       <Link href="/">
         <h1 className="text-lg font-bold">IdeaHub</h1>
       </Link>
